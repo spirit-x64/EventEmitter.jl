cd60748c6e5fadd12efa64f588dc996af9021899
Spirit <i8bou3@gmail.com>
2023-07-03 08:21:56 +0300

    + Structure types `Listener`, `Event`

--- src/EventEmitter.jl
+++ src/EventEmitter.jl
@@ -1,3 +1,25 @@
 module EventEmitter
 
+# exports
+export Listener, Event
+
+# Types
+struct Listener
+    callback::Function
+    once::Bool
+
+    Listener(cb::Function, once::Bool=false) = new(cb, once)
+end
+
+struct Event
+    name::Union{Symbol,AbstractString}
+    listeners::Vector{Listener}
+
+    function Event(n::Union{Symbol,AbstractString}, cbs::Function...; once::Bool=false)
+        new(n, [Listener(cb, once) for cb ∈ cbs])
+    end
+    Event(n::Union{Symbol,AbstractString}, l::Listener...) = new(n, [l...])
+    Event(n::Union{Symbol,AbstractString}) = new(n, [])
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using EventEmitter
 using Test
 
 @testset "EventEmitter.jl" begin
-    @test true
+    listener1 = Listener(() -> 1)
+    listener2 = Listener(() -> 2, true)
+    @test isa(listener1, Listener)
+    @test isa(listener2, Listener)
+
+    event1 = Event(:event1, () -> 3; once=false)
+    event2 = Event(:event2, Listener(() -> 4, true))
+    event3 = Event(:event3)
+    @test isa(event1, Event)
+    @test isa(event2, Event)
+    @test isa(event3, Event)
 end
